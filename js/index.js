dfedeed4adc43ae98359e046234e56f4867d431c
YSongYS <songyishu.yang@gmail.com>
2019-02-21 10:38:43 +0000

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much.";
+const token = '26639bb0d73e8ed74d0b609b3d3472cb89ab1f47';
+
+fetch('https://api.github.com/user/repos',{
+  headers:{
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
