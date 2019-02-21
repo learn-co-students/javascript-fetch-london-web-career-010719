f8bdc286be1bc63e43985443fb510714955ee2ca
kkoomin <minha.koo.9@gmail.com>
2019-02-21 00:00:20 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much.";
+
+// fetch('https://api.github.com/user/repos')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+
+const token = 'tokentokentokennnnnnn';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
