c4c244acaa9ab3d818ec33acdf18a5bedd1be111
studioshane <studioshane@gmail.com>
2019-02-22 09:55:49 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+
+const token = ' fb28d616f27aa6cb2e803f903a53697545a3ea59 ';
+fetch('https://api.github.com/user/repos', {
+    headers: {
+        Authorization: `token ${token}`
+    }
+})
+    .then(res => res.json())
+    .then(json => console.log(json));
+
+
+// fetch('https://api.github.com/user/repos')
+//     .then(res => res.json())
+//     .then(json => console.log(json));
