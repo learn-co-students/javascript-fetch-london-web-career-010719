54c9583c071df67dc02dbd5db28f1d79869ad63e
JoeyHere <josephodell@outlook.com>
2019-02-25 11:51:47 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-const token = 'aeababdf2ed1662bc99d6a0067a0553272215801';
+const token = '';
 fetch('https://api.github.com/user/repos', {
     headers: {
         Authorization: `token ${token}`
